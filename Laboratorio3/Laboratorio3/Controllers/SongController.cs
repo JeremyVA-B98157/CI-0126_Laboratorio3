2d7ed06b3416409259d8f01b184f3684d014595b
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-04-28 17:54:29 -0600

    Creación MVC para Song, además se añade Song en el menú de la página.

---
+++ Laboratorio3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,30 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = GetSong();
+            ViewBag.MainTitle = "My favorite song";
+            return View(song);
+        }
+
+        private SongModel GetSong()
+        {
+            SongModel song = (new SongModel
+            {
+                Album = "Sin miedo(del Amor y otros Demonios)",
+                NameSong = "Telepatía",
+                GenreSong = "Pop",
+                Interpreter = "Kali Uchis",
+                Likes = 1900000
+            });
+            return song;
+        }
+
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Models/SongModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public string Album { get; set; }
+        public string NameSong { get; set; }
+        public string GenreSong { get; set; }
+        public string Interpreter { get; set; }
+        public int Likes { get; set; }
+    }
+}
